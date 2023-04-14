7f2d2ad047cecb2b6a531da6a79cb78feaa81882
UncleDon <610175956@qq.com>
2023-04-14 20:48:38 +0800

    add chapter one code

---
+++ out_put_image/build/CMakeFiles/Image.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Image.dir/main.cpp.o"
+  "CMakeFiles/Image.dir/main.cpp.o.d"
+  "Image"
+  "Image.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
