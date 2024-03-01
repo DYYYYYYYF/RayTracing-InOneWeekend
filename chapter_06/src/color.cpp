44fe916fc7ac9d8eed60dc5d3d3a9f9133880d90
UncleDon <64484752+DYYYYYYYF@users.noreply.github.com>
2024-03-01 10:05:00 +0800

    Delete chapter_06 directory

--- chapter_06/src/color.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "color.h"
-
-void WriteColor(std::ostream &out, color pixel_color){
-    out << static_cast<int>(255.999 * pixel_color.x()) << " "
-        << static_cast<int>(255.999 * pixel_color.y()) << " "
-        << static_cast<int>(255.999 * pixel_color.z()) << std::endl;
-}
