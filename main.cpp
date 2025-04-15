4ace1ed6dddb244144d8b89400851dc19e47a56b
gituserdon <dongyf>
2025-04-15 14:50:10 +0800

    加保护。

--- main.cpp
+++ main.cpp
@@ -3,21 +3,22 @@
 #endif
 
 #include "renderer.h"
-#include "sphere.h"
-#include "material.h"
-
-#include <iostream>
-#include <fstream>
-
-using namespace std;
 
 int main()
 {
 	renderer* ray_tracer = new renderer();
-	ray_tracer->init();
-	ray_tracer->render();
-	ray_tracer->close();
-	delete(ray_tracer);
+	try
+	{
+		ray_tracer->init();
+		ray_tracer->render();
+		ray_tracer->close();
+		delete(ray_tracer);
+	}
+	catch (std::exception e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
 
 	return 0;
 }
